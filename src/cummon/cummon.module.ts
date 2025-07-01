2be109773a1df856c8564bcc26da01194b34de97
nanda <nandawijaya@apps.ipb.ac.id>
2025-07-02 02:24:36 +0700

    add validation service use zod

--- src/cummon/cummon.module.ts
+++ src/cummon/cummon.module.ts
@@ -2,6 +2,8 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
+import { PrismaService } from './prisma.service';
+import { ValidationService } from './validation.service';
 
 @Global()
 @Module({
@@ -15,6 +17,8 @@ import * as winston from 'winston';
         ConfigModule.forRoot({
             isGlobal: true,
         })
-    ]
+    ],
+    providers: [PrismaService, ValidationService],
+    exports: [PrismaService, ValidationService]
 })
 export class CummonModule {}
