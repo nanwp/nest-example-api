a0fb937ee6a7317c360bac9e842a63f7ba332a2a
nanda <nandawijaya@apps.ipb.ac.id>
2025-07-05 16:49:16 +0700

    fixing path prisma

--- src/cummon/prisma.service.ts
+++ src/cummon/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
-import { Prisma, PrismaClient } from "generated/prisma";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston"
 
