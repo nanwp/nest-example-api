f6dc54df3da820d6c51905a4df05bdf74ced24af
nanda <nandawijaya@apps.ipb.ac.id>
2025-07-07 22:42:25 +0700

    fixing tanggal cuti

--- src/cuti/cuti.validate.ts
+++ src/cuti/cuti.validate.ts
@@ -4,11 +4,11 @@ export class CutiValidation {
     static readonly CREATE_CUTI: ZodType = z.object({
         idJenisCuti: z.string().cuid(),
         idSubJenisCuti: z.string().cuid().optional(),
-        tanggalMulai: z.string().regex(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}$/, {
-            message: 'tanggalMulai must be in format YYYY-MM-DD HH:mm:ss.SSS'
+        tanggalMulai: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$/, {
+            message: 'tanggalMulai must be in format YYYY-MM-DDTHH:mm:ss.SSS'
         }),
-        tanggalSelesai: z.string().regex(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}$/, {
-            message: 'tanggalSelesai must be in format YYYY-MM-DD HH:mm:ss.SSS'
+        tanggalSelesai: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$/, {
+            message: 'tanggalSelesai must be in format YYYY-MM-DDTHH:mm:ss.SSS'
         }),
         keterangan: z.string().max(255).optional(),
         fileUrl: z.string().max(255).optional(),
